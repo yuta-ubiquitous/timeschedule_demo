fe7f2e2c77e8d612ce85553e1249d655a5581418
Yuta Takahashi <takahashi-yuta@kadokawa.jp>
2021-04-28 13:46:16 +0900

    dbにデータを追加

--- timeschedule/main.py
+++ timeschedule/main.py
@@ -1,10 +1,13 @@
+from datetime import datetime
+
 import sqlalchemy as sa
-from fastapi import FastAPI, Form, Request
+from fastapi import Depends, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from timeschedule import db
+from timeschedule.models import Schedule
 
 app = FastAPI()
 
@@ -35,9 +38,20 @@ async def read_item(request: Request):
 @app.post("/add", response_class=HTMLResponse)
 async def add_schedule(
     request: Request,
+    engine: sa.engine.Connectable = Depends(get_engine),
     name: str = Form(...),
-    datetime_start: str = Form(...),
-    datetime_end: str = Form(...),
+    datetime_start_str: str = Form(...),
+    datetime_end_str: str = Form(...),
 ):
-    print(name, datetime_start, datetime_end)
+
+    # 受け取った文字列の日時をdatetime型に変換
+    datetime_start = datetime.strptime(datetime_start_str, "%Y-%m-%d %H:%M")
+    datetime_end = datetime.strptime(datetime_end_str, "%Y-%m-%d %H:%M")
+
+    # dbに追加
+    db.add(
+        engine,
+        Schedule(name=name, datetime_start=datetime_start, datetime_end=datetime_end),
+    )
+
     return templates.TemplateResponse("add.html", {"request": request})
